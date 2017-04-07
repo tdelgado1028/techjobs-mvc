c010f7c2b538b70e78fd964cb3dcacddb31b507f
Tania Delgado <tdelgado1028@gmail.com>
2017-04-06 19:51:29 -0500

    End of project --- note to *COMMIT* more to then PUSH we want a history that is our friend

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
+import static org.launchcode.models.JobData.findByColumnAndValue;
+import static org.launchcode.models.JobData.findByValue;
 /**
  * Created by LaunchCode
  */
@@ -23,5 +26,18 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+    model.addAttribute("columns", ListController.columnChoices);
 
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<HashMap<String, String>>();
+        if (searchType.equals("all")) {
+            jobs = findByValue(searchTerm);
+        }else {
+            jobs = findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
